cdfd5ed3b551014d62fe1b7b4b89b743c135cb54
Najla Parker <najla08@gmail.com>
2022-06-03 17:53:24 -0600

    added new prop to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,32 @@ import React, { Component } from 'react';
 import NavItem from './NavItem';
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'NavItemActive': ''
+    }
+  }
+  activeitem = (x) => {
+    if (this.state.NavItemActive.length > 0) {
+      document.getElementById(this.state.NavItemActive).classList.remove('active');
+    }
+    this.setState({ 'NavItemId': x }, () => {
+      document.getElementById(this.state.NavItemActive).classList.add('active');
+    });
+  };
   render() {
     return (
       <nav>
         <ul>
-          <NavItem item="Home" tolink="/" ></NavItem>
-          <NavItem item="About" tolink="/about" ></NavItem>
-          <NavItem item="Education" tolink="/education"></NavItem>
-          <NavItem item="Skills" tolink="/skills"></NavItem>
-          <NavItem item="Contact" tolink="/contact"></NavItem>
+          <NavItem item="Home" tolink="/" activec={this.activeitem}></NavItem>
+          <NavItem item="About" tolink="/about" activec={this.activeitem}></NavItem>
+          <NavItem item="Education" tolink="/education" activec={this.activeitem}></NavItem>
+          <NavItem item="Skills" tolink="/skills" activec={this.activeitem}></NavItem>
+          <NavItem item="Contact" tolink="/contact" activec={this.activeitem}></NavItem>
         </ul>
       </nav>
     )
   }
 }
-
-export default Navbar;
+export default Navbar
