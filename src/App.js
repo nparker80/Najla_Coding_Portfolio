1151de648e08031555c3c8760605397361f53ea8
Najla Parker <najla08@gmail.com>
2022-06-04 11:20:34 -0600

    Navbar setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div>
-
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Navbar />
+      </div>
+    </Router>
+  )
 }
-
 export default App;
