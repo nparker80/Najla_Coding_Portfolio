4ab7759b43e28a277e7a2d5bbbe5ec158fc931c7
Najla Parker <najla08@gmail.com>
2022-06-08 20:10:58 -0600

    Update routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './contents/Home';
 import About from './contents/About';
 import Skills from './contents/Skills';
+import Contact from './contents/Contact';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </BrowserRouter>
