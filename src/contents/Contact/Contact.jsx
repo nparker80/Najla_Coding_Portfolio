5a75bc0120b5dcc05c37ddb6542586bff774c57b
Najla Parker <najla08@gmail.com>
2022-06-15 16:24:37 -0600

    added paragraph class to Contact

--- src/contents/Contact/Contact.jsx
+++ src/contents/Contact/Contact.jsx
@@ -5,8 +5,7 @@ import './Contact.css';
 class Contact extends Component {
   render() {
     return (
-      <div className="condiv">
-        <p>Thank you for visiting my website.<br></br>My projects are ever changing so check back soon for updates!</p>
+      <div className="condiv"><p className="contactP">Thank you for visiting my website.<br></br>My projects are ever changing so check back soon for updates!</p>
         <h1 className="subtopic">Contact Me</h1>
         <h3>Email: najla08@gmail.com</h3>
         <Social />
