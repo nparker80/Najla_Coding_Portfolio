eadd5786228b23c12a13190d44bc5061956fb73d
Najla Parker <najla08@gmail.com>
2022-06-15 16:15:09 -0600

    added Contact styling additional information

--- src/contents/Contact/Contact.jsx
+++ src/contents/Contact/Contact.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Social from '../../components/Social/Social';
+import './Contact.css';
 
 class Contact extends Component {
   render() {
     return (
       <div className="condiv">
+        <p>Thank you for visiting my website.<br></br>My projects are ever changing so check back soon for updates!</p>
         <h1 className="subtopic">Contact Me</h1>
-        <h3>Email  :   najla08@gmail.com</h3>
+        <h3>Email: najla08@gmail.com</h3>
         <Social />
-      </div>
+      </div >
     )
   }
 }
