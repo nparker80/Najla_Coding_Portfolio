f6767252c792e4b303b1da0a6e389f127b633092
Najla Parker <najla08@gmail.com>
2022-06-15 17:06:36 -0600

    worked on Skill content

--- src/contents/Skills/Skills.jsx
+++ src/contents/Skills/Skills.jsx
@@ -1,4 +1,5 @@
 import Skill from './Skill'
+import './Skills.css';
 
 function Skills() {
   return (
@@ -12,6 +13,18 @@ function Skills() {
         <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="The logo icon for GitHub" title="GitHub" />
         <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt="The logo icon for MongoDB" title="MongoDB" />
         <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="The logo icon for NodeJS" title="Node JS" />
+        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="The logo icon for Bootstrap" title="Bootstrap" />
+        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt="The logo icon for Express" title="Express" />
+        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="The logo icon for GitHub" title="GitHub" />
+        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original-wordmark.svg" alt="The logo icon for Heroku" title="Heroku" />
+        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg" alt="The logo icon for GraphQL" title="GraphQL" />
+        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-plain-wordmark.svg" alt="The logo icon for jQuery" title="jQuery" />
+        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" alt="The logo icon for Jest" title="Jest" />
+        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg" alt="The logo icon for MySQL" title="MySQL" />
+        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" alt="The logo icon for NPM" title="NPM" />
+        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="The logo icon for NodeJS" title="Node JS" />
+        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" alt="The logo icon for Visual Studio Code" title="VS Code" />
+        <Skill source="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" alt="The logo icon for Sequelize" title="Sequelize" />
       </div>
     </div>
   )
