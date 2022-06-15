8070e8fede64251e84a1216257b509f0e3b8bb5e
Najla Parker <najla08@gmail.com>
2022-06-15 15:13:16 -0600

    about styling and color palette

--- src/contents/About/About.jsx
+++ src/contents/About/About.jsx
@@ -4,39 +4,40 @@ class About extends Component {
   render() {
     return (
       <div className="condiv">
+        <div className="container">
+          <h1 className="subtopic">About Me</h1>
+          <br></br>
+          <p> Welcome! My name is Najla Parker and I am currently working as a program planner in the Program
+            Management
+            Organization within Raytheon Technologies. I have spent the last 7 years working in the defense
+            contractor
+            industry
+            supporting various types of programs and honing my project management skills.
+          </p>
 
-        <h1 className="subtopic">About Me</h1>
-        <br></br>
-        <p> Welcome! My name is Najla Parker and I am currently working as a program planner in the Program
-          Management
-          Organization within Raytheon Technologies. I have spent the last 7 years working in the defense
-          contractor
-          industry
-          supporting various types of programs and honing my project management skills.
-        </p>
+          <p> After working with
+            software
+            development teams and participating in the timing of releases, increment planning and observing the
+            backlog grooming
+            sessions; I decided it was my turn! This is what led me to join the 24 week Coding Bootcamp at The
+            University of Denver.
+          </p>
 
-        <p> After working with
-          software
-          development teams and participating in the timing of releases, increment planning and observing the
-          backlog grooming
-          sessions; I decided it was my turn! This is what led me to join the 24 week Coding Bootcamp at The
-          University of Denver.
-        </p>
+          <p> I have acquired skills in the fundamental concepts of web development (HTML, CSS, and JavaScript), as
+            well
+            as command line fundamentals and APIs. In addition, I am learning to engineer a full-stack web
+            application, working with servers, databases, and other back end technologies, and connecting them to the
+            front end.
+          </p>
 
-        <p> I have acquired skills in the fundamental concepts of web development (HTML, CSS, and JavaScript), as
-          well
-          as command line fundamentals and APIs. In addition, I am learning to engineer a full-stack web
-          application, working with servers, databases, and other back end technologies, and connecting them to the
-          front end.
-        </p>
-
-        <p>
-          I look forward to a lifetime of learning and evolving as well as the opportunity to problem solve. These
-          are my favorite aspects of
-          being a developer thus far and something I employ daily in my current role. As I continue the journey to
-          become a
-          full stack developer, I am keeping an open mind and trying to discover what I most enjoy.
-        </p>
+          <p>
+            I look forward to a lifetime of learning and evolving as well as the opportunity to problem solve. These
+            are my favorite aspects of
+            being a developer thus far and something I employ daily in my current role. As I continue the journey to
+            become a
+            full stack developer, I am keeping an open mind and trying to discover what I most enjoy.
+          </p>
+        </div>
       </div>
     )
   }
