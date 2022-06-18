8290a97c4d99ed4c41006400b606bbd36a5d5276
Najla Parker <najla08@gmail.com>
2022-06-18 09:51:25 -0600

    cleanup nested scss

--- src/contents/Contact/Contact.jsx
+++ src/contents/Contact/Contact.jsx
@@ -5,7 +5,7 @@ import './Contact.scss';
 class Contact extends Component {
   render() {
     return (
-      <div class="condiv-contact" id="app__container">
+      <div className="condiv-contact" id="app__container">
         <div className="contact__content">
           <p>Thank you for visiting my website.<br></br>My projects are ever changing so check back soon for updates!</p>
           <h1 className="subtopic">Contact Me</h1>
@@ -13,6 +13,7 @@ class Contact extends Component {
           <Social />
         </div >
       </div >
+
     )
   }
 }
