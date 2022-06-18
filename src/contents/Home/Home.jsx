bf8d259733059cdfbf825c930156303bf6b8df71
Najla Parker <najla08@gmail.com>
2022-06-17 22:19:42 -0600

    media queries Home final?

--- src/contents/Home/Home.jsx
+++ src/contents/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactTypingEffect from 'react-typing-effect';
 import profilepic from '../../assets/najla.jpg';
 import './Home.scss';
 import Social from '../../components/Social/Social';
@@ -7,15 +6,16 @@ import Social from '../../components/Social/Social';
 class Home extends Component {
   render() {
     return (
-      <div className="condiv-home">
-
-        <img src={profilepic} className='profilepic' alt='Najlapic' />
-        <h1 className="introduction">Hey and hi,<span>
-          <br></br>
-          My name is Najla and I am a full stack web developer.</span></h1>
-        <ReactTypingEffect className="typingeffect" text={['Welcome to my portfolio!']} speed={100} eraseDelay={2000} />
-        <Social />
-      </div>
+      <div className="condiv-home" id="app__container">
+        <div className="home__content">
+          <img src={profilepic} alt='Najlapic' />
+          <h1 className="introduction">Hey and hi,<span>
+            <br></br>
+            My name is Najla and I am a full stack web developer.</span></h1>
+          <p>Welcome to my portfolio!</p>
+          <Social />
+        </div>
+      </div >
     )
   }
 }
