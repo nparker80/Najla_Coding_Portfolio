71793ac15e9cd1f93cbdb364bb6f10ffe1c08071
Najla Parker <najla08@gmail.com>
2022-06-21 17:32:53 -0600

    publish

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -5,9 +5,9 @@ class Social extends Component {
   render() {
     return (
       <div class="social">
-        <a href="https://www.github.com/nparker80" target="_blank"><i class="fa fa-github"></i></a>
+        <a href="https://www.github.com/nparker80" target="_blank" rel="noopener"><i class="fa fa-github"></i></a>
 
-        <a href="https://www.linkedin.com/in/najlaparker" target="_blank"><i class="fa fa-linkedin"></i></a>
+        <a href="https://www.linkedin.com/in/najlaparker" target="_blank" rel="noopener"><i class="fa fa-linkedin"></i></a>
       </div>
     )
   }
