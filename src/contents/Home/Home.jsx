b9dab1b747e7a6cdbcaf57e80e1287f600cbc27c
Najla Parker <najla08@gmail.com>
2022-06-21 14:40:37 -0600

    media queries about

--- src/contents/Home/Home.jsx
+++ src/contents/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import profilepic from '../../assets/najla.png';
+import profilepic from '../../assets/najla.png';
 import './Home.scss';
 import Social from '../../components/Social/Social';
 
@@ -7,13 +7,14 @@ class Home extends Component {
   render() {
     return (
       <div className="condiv-home" id="app__container">
-        {/* <img src={profilepic} alt='Najlapic' /> */}
+        <img src={profilepic} alt='Najlapic' />
         <div className="greeting">
           <p className="p-text-home">Welcome to my portfolio!</p><h1>Hey and hi,<span>
             <br></br>
-            My name is Najla and I am a full stack web developer.</span></h1>
-          <Social />
-        </div></div >
+            My name is Najla and I am a full stack web developer.</span></h1></div>
+        <div className="icons"><Social /></div>
+      </div>
+
 
     )
   }
